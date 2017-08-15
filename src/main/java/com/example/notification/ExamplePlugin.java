f0e74aab4e9b3f2bb03ed1c631673bff4ed670b4
Chris Atkin <atkinchris@gmail.com>
2017-08-16 00:19:17 +0100

    Add basic http post

--- src/main/java/com/example/notification/ExamplePlugin.java
+++ src/main/java/com/example/notification/ExamplePlugin.java
@@ -6,10 +6,29 @@ import com.thoughtworks.go.plugin.api.exceptions.*;
 import com.thoughtworks.go.plugin.api.request.*;
 import com.thoughtworks.go.plugin.api.response.*;
 import java.util.*;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 
 @Extension
 public class ExamplePlugin implements GoPlugin {
   private GoApplicationAccessor accessor;
+
+  private void postMessage(String message) {
+    try {
+      HttpClient httpClient = HttpClientBuilder.create().build();
+      StringEntity requestEntity = new StringEntity(message, "application/json");
+      HttpPost postMethod = new HttpPost("http://localhost:3000");
+
+      postMethod.setEntity(requestEntity);
+      httpClient.execute(postMethod);
+    } catch (Exception ex) {
+      //handle exception here
+    }
+  }
 
   public void initializeGoApplicationAccessor(GoApplicationAccessor accessor) {
     this.accessor = accessor;
@@ -24,7 +43,7 @@ public class ExamplePlugin implements GoPlugin {
       case "notifications-interested-in":
           return new DefaultGoPluginApiResponse(200, "{\"notifications\":[\"stage-status\"]}");
       case "stage-status":
-          System.out.println(request.requestBody());
+          postMessage(request.requestBody());
           return new DefaultGoPluginApiResponse(200, "{\"status\": \"success\"}");
       default:
           throw new UnhandledRequestTypeException(request.requestName());
